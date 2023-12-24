15f37184064f53c7352c3b449fa2cb179938be5d
1-Dex <1dex@blockto.ir>
2023-12-24 15:30:51 +0330

    Initial commit

---
+++ KingOfEther.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// Dos || Denial of Service
+
+// Bad contract
+contract KingOfEther {
+    address public king;
+    uint256 public balance;
+
+    function claimThrone() external payable {
+        require(msg.value > balance, "Need to pay more to become the king");
+
+        (bool sent,) = king.call{value: balance}("");
+        require(sent, "Failed to send Ether");
+
+        balance = msg.value;
+        king = msg.sender;
+    }
+}
+
+// Good contract
+contract KingOfEther {
+    address public king;
+    uint256 public balance;
+    mapping(address => uint256) public balances;
+
+    function claimThrone() external payable {
+        require(msg.value > balance, "Need to pay more to become the king");
+
+        balances[king] += balance;
+
+        balance = msg.value;
+        king = msg.sender;
+    }
+
+    function withdraw() public {
+        require(msg.sender != king, "Current king cannot withdraw");
+
+        uint256 amount = balances[msg.sender];
+        balances[msg.sender] = 0;
+
+        (bool sent,) = msg.sender.call{value: amount}("");
+        require(sent, "Failed to send Ether");
+    }
+}
+
+contract Attack {
+    KingOfEther kingOfEther;
+
+    constructor(KingOfEther _kingOfEther) {
+        kingOfEther = KingOfEther(_kingOfEther);
+    }
+
+    function attack() public payable {
+        kingOfEther.claimThrone{value: msg.value}();
+    }
+}
